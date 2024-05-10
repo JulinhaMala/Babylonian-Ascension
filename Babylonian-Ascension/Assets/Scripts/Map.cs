66f13ffd059b53a4764abf7f6726ee178dfec4c2
Richardbertan <Richardbertan18@gmail.com>
2024-05-10 11:40:08 -0300

    Tentativa de zoom

---
+++ Babylonian-Ascension/Assets/Scripts/Map.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject map;
+    float currentScale, maxScale, minScale;
+    RectTransform imageToZoom;
+    void Update()
+    {
+        Zoom(Input.GetAxis("Mouse ScrollWheel"));
+    }
+
+    void Zoom(float increment)
+    {
+        currentScale += increment;
+        if (currentScale >= maxScale)
+        {
+            currentScale = maxScale;
+        }
+        else if (currentScale <= minScale)
+        {
+            currentScale = minScale;
+        }
+        imageToZoom.localScale = new Vector2(currentScale, currentScale);
+    }
+
+}
