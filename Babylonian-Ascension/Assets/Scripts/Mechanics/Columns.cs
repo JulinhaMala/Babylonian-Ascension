7b54b77ed58c3b7b4f9cfca71e376038311f031e
Richardbertan <Richardbertan18@gmail.com>
2024-06-23 21:37:28 -0300

    beta test

--- Babylonian-Ascension/Assets/Scripts/Mechanics/Columns.cs
+++ Babylonian-Ascension/Assets/Scripts/Mechanics/Columns.cs
@@ -7,12 +7,7 @@ public class Columns : MonoBehaviour
     [SerializeField] public Image ColuEco, ColuMil, ColuSoci, ColuDese;
     [SerializeField] public TMP_Text Eco_Text, Mil_Text, Soci_Text, Dese_Text;
 
-    private void FixedUpdate()
-    {
-        AllSet();
-    }
-
-    public void AllSet()
+    private void Update()
     {
         EcoSet();
         MilSet();
@@ -24,7 +19,7 @@ public class Columns : MonoBehaviour
         DeseText();
     }
 
-    public void EcoSet()
+    void EcoSet()
     {
         ColuEco.fillAmount = Stats.instance.econo / Stats.instance.maxEcono;
         if ((Stats.instance.econo / Stats.instance.maxEcono) <= 0.1)
@@ -32,7 +27,7 @@ public class Columns : MonoBehaviour
             ColuEco.fillAmount = 0.1f;
         }
     }
-    public void MilSet()
+    void MilSet()
     {
         ColuMil.fillAmount = Stats.instance.milita / Stats.instance.maxMilita;
         if ((Stats.instance.milita / Stats.instance.maxMilita) <= 0.1)
@@ -40,7 +35,7 @@ public class Columns : MonoBehaviour
             ColuMil.fillAmount = 0.1f;
         }
     }
-    public void SociSet()
+    void SociSet()
     {
         ColuSoci.fillAmount = Stats.instance.social / Stats.instance.maxSocial;
         if ((Stats.instance.social / Stats.instance.maxSocial) <= 0.1)
@@ -48,7 +43,7 @@ public class Columns : MonoBehaviour
             ColuSoci.fillAmount = 0.1f;
         }
     }
-    public void DeseSet()
+    void DeseSet()
     {
         ColuDese.fillAmount = Stats.instance.desenvol / Stats.instance.maxDesenvol;
         if ((Stats.instance.desenvol / Stats.instance.maxDesenvol) <= 0.1)
@@ -58,19 +53,19 @@ public class Columns : MonoBehaviour
     }
 
     
-    public void EcoText()
+    void EcoText()
     {
         Eco_Text.text = $" {Stats.instance.econo} / {Stats.instance.maxEcono}";
     }
-    public void MilText()
+    void MilText()
     {
         Mil_Text.text = $"{Stats.instance.milita} / {Stats.instance.maxMilita}";
     }
-    public void SociText()
+    void SociText()
     {
         Soci_Text.text = $"{Stats.instance.social} / {Stats.instance.maxSocial}";
     }
-    public void DeseText()
+    void DeseText()
     {
         Dese_Text.text = $"{Stats.instance.desenvol} / {Stats.instance.maxDesenvol}";
     }
