3a0a00afd056c7298abef888e361816ca49ef2ad
Richardbertan <Richardbertan18@gmail.com>
2024-10-14 16:03:44 -0300

    Att bether UI

--- Sovereign Reign/Assets/Scripts/Game/Events/EventTimer.cs
+++ Sovereign Reign/Assets/Scripts/Game/Events/EventTimer.cs
@@ -24,7 +24,6 @@ public class EventTimer : MonoBehaviour
             }
             SetActive.Invoke();
             Stats.instance.didAllEvent = false;
-            
         }
     }
     public void Reset()
