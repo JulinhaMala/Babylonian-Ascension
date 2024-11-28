612be2b58ecd77e363047e644314062c5df7e203
Richardbertan <Richardbertan18@gmail.com>
2024-11-27 21:31:28 -0300

    Att Menu Moving

--- Sovereign Reign/Assets/Scripts/Global/Tutorial.cs
+++ Sovereign Reign/Assets/Scripts/Global/Tutorial.cs
@@ -7,7 +7,11 @@ public class Tutorial : MonoBehaviour
 {
     public int index;
 
+    public UnityEvent deactiveAll;
+
     public UnityEvent trade1;
+
+    public UnityEvent colluns1;
 
 
     public void PassTutorial()
@@ -15,23 +19,49 @@ public class Tutorial : MonoBehaviour
         switch (index)
         {
             case 0:
+                deactiveAll.Invoke();
+
                 trade1.Invoke();
                 break;
             case 1:
+                deactiveAll.Invoke();
+
+                colluns1.Invoke();
                 break;
             case 2:
+                deactiveAll.Invoke();
+
+                trade1.Invoke();
                 break;
             case 3:
+                deactiveAll.Invoke();
+
+                trade1.Invoke();
                 break;
             case 4:
+                deactiveAll.Invoke();
+
+                trade1.Invoke();
                 break;
             case 5:
+                deactiveAll.Invoke();
+
+                deactiveAll.Invoke();
                 break;
             case 6:
+                deactiveAll.Invoke();
+
+                deactiveAll.Invoke();
                 break;
             case 7:
+                deactiveAll.Invoke();
+
+                deactiveAll.Invoke();
                 break;
             case 8:
+                deactiveAll.Invoke();
+
+                deactiveAll.Invoke();
                 break;
             case 9:
                 break;
